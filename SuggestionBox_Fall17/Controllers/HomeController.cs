c50881f65b52ffa29f037d58b7a9a5e2a1503246
Jarryd Huntley <Jarryd Huntley>
2017-10-24 10:12:40 -0400

    Removed the extra controller actions.

--- SuggestionBox_Fall17/Controllers/HomeController.cs
+++ SuggestionBox_Fall17/Controllers/HomeController.cs
@@ -10,21 +10,9 @@ namespace SuggestionBox_Fall17.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
